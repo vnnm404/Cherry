44ddb1137e01f40aa34a641f7a08b03bba5e3d1f
herocharge <techwithbhargav@gmail.com>
2022-12-07 22:20:03 +0530

    [Add] views, public folders and basic game functionality

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Cherry
+
+An online multiplayer (2-player) chess game.
+
+## TODOs
+- [ ] Client to Server communication
+- [ ] Validation of moves
+- [ ] Sign up and Login
+- [ ] Player1 - server - Player2 communication
+- [ ] matchmaking
+- [ ] generating links for matches
+- [ ] Make it pretty 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import express from 'express';
+const express = require('express');
+var path = require('path');
 
 const app = express();
 const port = 5500;
 
 app.use(express.static('./public'));
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.render('index');
 });
 
 app.listen(port, () => {
