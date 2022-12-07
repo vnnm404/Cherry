38d8905223786be2d1dff27737b68d12ac0a286e
vnnm404 <vennam404@gmail.com>
2022-12-07 22:33:27 +0530

    fixed some stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-var path = require('path');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const PORT = 5500;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('./public'));
-app.engine('.html', require('ejs').__express);
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('./src/public'));
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
 
 
 app.get('/', (req, res) => {
