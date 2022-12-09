b2cf783267096fe1438e7f96417619a2e966199f
herocharge <techwithbhargav@gmail.com>
2022-12-09 12:06:58 +0530

    [Fix] typo

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -61,9 +61,9 @@ export function chessMoveValidate(board, moveFromCoord, moveToCoord, turn){
 
     let valid = true;
 
-    valid = valide && coordValidate(moveFromCoord);
-    valid = valide && coordValidate(moveToCoord);
-    valid = valide && turnValidate(turn);
+    valid = valid && coordValidate(moveFromCoord);
+    valid = valid && coordValidate(moveToCoord);
+    valid = valid && turnValidate(turn);
 
     // move to same position not a move
     if (coordEqual(moveFromCoord, moveToCoord)){
