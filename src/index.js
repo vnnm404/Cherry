e77fa5a177880975ce6d097ba117f9af911447c9
vnnm404 <vennam404@gmail.com>
2022-12-10 13:18:32 +0530

    format

--- src/index.js
+++ src/index.js
@@ -15,20 +15,19 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 
-
 app.use(express.static(path.dirname(__filename) + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', path.dirname(__filename) + '/views');
 
 
 let boardState = [[12, 14, 13, 11, 10, 13, 14, 12],
-                    [9, 9, 9, 9, 9, 9, 9, 9],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [4, 6, 5, 3, 2, 5, 6, 4]];
+  [9, 9, 9, 9, 9, 9, 9, 9],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [4, 6, 5, 3, 2, 5, 6, 4]];
 let turnState = 0;
 
 
@@ -40,13 +39,13 @@ io.on('connection', socket => {
   console.log(`User[${socket.id}]: connected`);
   turnState = 0;
   boardState = [[12, 14, 13, 11, 10, 13, 14, 12],
-                    [9, 9, 9, 9, 9, 9, 9, 9],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [1, 1, 1, 1, 1, 1, 1, 1],
-                    [4, 6, 5, 3, 2, 5, 6, 4]];
+    [9, 9, 9, 9, 9, 9, 9, 9],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [4, 6, 5, 3, 2, 5, 6, 4]];
   socket.on('move', (fromCoords, toCoords) => {
     console.log(`User[${socket.id}]: sent: ${fromCoords.x} ${fromCoords.y} || ${toCoords.x} ${toCoords.y}`);
 
@@ -54,28 +53,28 @@ io.on('connection', socket => {
       valid move = validateMove(board, move_from, move_to, turn (black or white));
 
       if valid move:
-        // success
+    // success
         socket.emit('validated', 1);
       else:
-        // failure
+  // failure
         socket.emit('validated', 0);
 
-    */
+*/
 
-    // console.log('turn ::: ' + turnState);
-    console.log('Validating: ' + boardState[fromCoords.y][fromCoords.x]);
+  // console.log('turn ::: ' + turnState);
+  console.log('Validating: ' + boardState[fromCoords.y][fromCoords.x]);
 
-    let valid = chessMoveValidate(boardState, fromCoords, toCoords, turnState);
-    console.log(turnState + ': Move validated to be:: ' + valid);
+  let valid = chessMoveValidate(boardState, fromCoords, toCoords, turnState);
+  console.log(turnState + ': Move validated to be:: ' + valid);
 
-    if (valid) {
-      boardState[toCoords.y][toCoords.x] = boardState[fromCoords.y][fromCoords.x];
-      boardState[fromCoords.y][fromCoords.x] = 0;
-      turnState = 1 - turnState;
-      socket.emit('validated', 1);
-    } else {
-      socket.emit('validated', 0);
-    }
+  if (valid) {
+    boardState[toCoords.y][toCoords.x] = boardState[fromCoords.y][fromCoords.x];
+    boardState[fromCoords.y][fromCoords.x] = 0;
+    turnState = 1 - turnState;
+    socket.emit('validated', 1);
+  } else {
+    socket.emit('validated', 0);
+  }
   });
 
   socket.on('auth', ({ username, password }) => {
@@ -87,7 +86,7 @@ io.on('connection', socket => {
       } else {
         socket.emit('auth', 0);
       }
-    */
+      */
   });
 
   socket.on('disconnect', () => {
